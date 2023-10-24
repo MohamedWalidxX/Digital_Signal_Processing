1ab510d6ef25dd9b7a62987afa0a36f5541927e8
MohamedWalidxX <Moh.Walid2002@gmail.com>
2023-10-24 20:34:15 +0300

    Arithmetic_package_DSP
    Arithmetic package implementation for Signals like Add,subtract,Shift

--- task1_package/ArithmeticOperations.py
+++ task1_package/ArithmeticOperations.py
@@ -4,12 +4,70 @@ import matplotlib.pyplot as plt
 import sino_waves as si
 import cosine_wave as cs
 from tkinter import messagebox
-
+from draw import draw_signal2
 from tkinter import *
 
-import numpy as np
-import matplotlib.pyplot as plt
 
+def equalize_arrays(arr1, arr2):
+    condition = 0  # which is the smallest variable
+    if len(arr1) < len(arr2):
+        arr1.extend([0] * (len(arr2) - len(arr1)))
+        condition = 1
+    elif len(arr2) < len(arr1):
+        arr2.extend([0] * (len(arr1) - len(arr2)))
+        condition = 2
+    return arr1, arr2, condition
+
+
+def readFile_returnArray(path):
+    x_values = []
+    y_values = []
+    # Read the data from the file, skipping the first three lines
+    with open(path, "r") as file:
+        lines = file.readlines()[3:]  # Skip the first three lines
+        for line in lines:
+            x, y = map(float, line.strip().split())
+            x_values.append(x)
+            y_values.append(y)
+    return x_values, y_values
+
+
+def addSignals(path1, path2):
+    x1, y1 = readFile_returnArray(path1)
+    x2, y2 = readFile_returnArray(path2)
+    y1, y2, condition = equalize_arrays(y1, y2)
+    y_plot = np.add(y1, y2)
+    if condition == 1:
+        draw_signal2(x2, y_plot)
+    else:
+        draw_signal2(x1, y_plot)
+
+
+def subtractSignals(path1, path2):
+    x1, y1 = readFile_returnArray(path1)
+    x2, y2 = readFile_returnArray(path2)
+    y1, y2, condition = equalize_arrays(y1, y2)  # extend the samllest array with zeroes
+    y_plot = abs(np.subtract(y1,y2))
+    if condition == 1:
+        draw_signal2(x2, y_plot)
+    else:
+        draw_signal2(x1, y_plot)
+
+
+def multiplySignal(path, constNum):
+    x1, y1 = readFile_returnArray(path)
+    y1 = np.array(y1) * constNum
+    draw_signal2(x1, y1)
+
+def square_signal(path):
+    x1, y1 = readFile_returnArray(path)
+    y1 = np.multiply(y1,y1)
+    draw_signal2(x1, y1)
+
+def shift_signal(path, shiftAmount):
+    x1, y1 = readFile_returnArray(path)
+    x1 = np.array(x1) - shiftAmount
+    draw_signal2(x1, y1)
 
 def normalize(path, a, b):
     # Read the signal file
@@ -34,10 +92,6 @@ def normalize(path, a, b):
     plt.show()
 
 
-import numpy as np
-import matplotlib.pyplot as plt
-
-
 def accumulate(path):
     # Read the signal file
     data = np.loadtxt(path)
@@ -56,4 +110,3 @@ def accumulate(path):
     plt.show()
 
     return accumulated_y
-
--- task1_package/testAll.py
+++ task1_package/testAll.py
@@ -1,15 +1,13 @@
+import numpy as np
+
 from draw import draw_signal
 from cosine_wave import generate_cosine_signal
 from sino_waves import generate_sinusoidal_signal
 from comparesignals import SignalSamplesAreEqual
+from ArithmeticOperations import *
 
 
-button = 2
-if button == 1:
-    draw_signal("signal1.txt",'m')
-elif button == 2:
-    t,sig = generate_sinusoidal_signal(3,1.96349540849362,360,720,1)
-    SignalSamplesAreEqual("SinOutput.txt",0,sig)
-else:
-    t,sig = generate_cosine_signal(3,2.35619449019235,200,500,1)
-    SignalSamplesAreEqual("CosOutput.txt",0,sig)
+directory = "inOut/task2/"
+signalFile = "Signal1.txt"
+signalFile2 = "Signal2.txt"
+shift_signal(directory+signalFile, 500)
